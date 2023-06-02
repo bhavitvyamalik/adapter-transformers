89039c0abfec20635c66ac74ee00cf19f4dd82ff
Bhavitvya Malik <bhavitvya.malik@gmail.com>
2023-06-02 12:00:29 +0100

    layers change to HubertEncoder

--- src/transformers/adapters/mixins/hubert.py
+++ src/transformers/adapters/mixins/hubert.py
@@ -30,7 +30,7 @@ class HubertModelAdaptersMixin(ModelAdaptersMixin):
     """Adds adapters to the Hubert module."""
 
     def iter_layers(self) -> Iterable[Tuple[int, nn.Module]]:
-        for i, layer in enumerate(self.BaseModelOutput.layer):
+        for i, layer in enumerate(self.HubertEncoder.layers):
             yield i, layer
 
 
