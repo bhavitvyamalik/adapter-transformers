37b8fa75e8cabb2f6ccc98faabb3483582eddb62
Bhavitvya Malik <bhavitvya.malik@gmail.com>
2023-06-14 21:57:49 +0100

    remove print statements

--- src/transformers/adapters/layer.py
+++ src/transformers/adapters/layer.py
@@ -530,9 +530,6 @@ class AdapterLayer(AdapterLayerBase, nn.Module):
         # Replicate in both directions as residual might be larger (e.g. GPT-J)
         (hidden_states,) = adjust_tensors_for_parallel(residual_input, hidden_states)
         adapter_setup = self.get_active_setup(self.adapters)
-        print(adapter_setup, "adapter_setup")
-        # hard code
-        # adapter_setup = None
         if adapter_setup is not None:
             input_hidden_states = hidden_states
 
