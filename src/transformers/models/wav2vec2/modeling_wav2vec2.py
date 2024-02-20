742a4bcc0c81128d0902482b64e5c58ef06456b2
Bhavitvya Malik <bhavitvya.malik@gmail.com>
2024-02-20 16:39:47 +0000

    add layer norm

--- src/transformers/models/wav2vec2/modeling_wav2vec2.py
+++ src/transformers/models/wav2vec2/modeling_wav2vec2.py
@@ -765,7 +765,7 @@ class Wav2Vec2EncoderLayerStableLayerNorm(Wav2Vec2EncoderLayerStableLayerNormAda
         ffn_output = self.feed_forward(ffn_output)  # (bs, seq_length, dim)
 
         ffn_output: torch.Tensor = self.output_adapters( # trying something pls work
-            ffn_output, sa_output, None
+            ffn_output, sa_output, self.final_layer_norm
         )  # (bs, seq_length, dim)
 
         outputs = (ffn_output,)
