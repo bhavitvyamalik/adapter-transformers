f9801c9727d080bb54af6c1edeed3a9df4fd305d
Bhavitvya Malik <bhavitvya.malik@gmail.com>
2024-03-04 18:05:05 +0000

    change lr scheduler

--- src/transformers/optimization.py
+++ src/transformers/optimization.py
@@ -75,7 +75,8 @@ def get_constant_schedule_with_warmup(optimizer: Optimizer, num_warmup_steps: in
 def get_linear_schedule_with_warmup(optimizer, num_warmup_steps, num_training_steps, last_epoch=-1):
     """
     Create a schedule with a learning rate that decreases linearly from the initial lr set in the optimizer to 0, after
-    a warmup period during which it increases linearly from 0 to the initial lr set in the optimizer.
+    a warmup period and constant during which it increases linearly from 0 to the initial lr set in the optimizer and stays 
+    constant respectively. This is done inorder to reproduce results from https://arxiv.org/abs/2111.09296.
 
     Args:
         optimizer ([`~torch.optim.Optimizer`]):
@@ -94,6 +95,8 @@ def get_linear_schedule_with_warmup(optimizer, num_warmup_steps, num_training_st
     def lr_lambda(current_step: int):
         if current_step < num_warmup_steps:
             return float(current_step) / float(max(1, num_warmup_steps))
+        elif current_step >= num_warmup_steps and current_step < int(0.4 * num_training_steps):
+            return 1
         return max(
             0.0, float(num_training_steps - current_step) / float(max(1, num_training_steps - num_warmup_steps))
         )
